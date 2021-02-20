3235b3782d36db6b76a94cfedeb8cbbbbe554e0f
kodakoda0921 <y.m1911a1.0921@gmail.com>
2021-02-20 17:38:50 +0900

    [fix]コメントフォームの作成

--- frontend/components/TwitterCard.vue
+++ frontend/components/TwitterCard.vue
@@ -51,17 +51,26 @@
             :src="current_user.user.image"
           ></v-img>
         </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Evan You</v-list-item-title>
-        </v-list-item-content>
-
+        <!--        <v-list-item-content> </v-list-item-content>-->
         <v-row align="center" justify="end">
-          <v-icon class="mr-1"> mdi-heart</v-icon>
-          <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
-          <v-icon class="mr-1"> mdi-share-variant</v-icon>
-          <span class="subheading">45</span>
+          <v-col cols="8">
+            <v-card-text>
+              <v-text-field
+                v-model="content"
+                :size="1"
+                label="コメントフォーム"
+              >
+              </v-text-field>
+            </v-card-text>
+          </v-col>
+          <v-col cols="2">
+            <v-icon class="mr-1"> mdi-heart</v-icon>
+            <span class="subheading mr-2">256</span>
+          </v-col>
+          <v-col cols="2">
+            <v-icon class="mr-1"> mdi-share-variant</v-icon>
+            <span class="subheading">45</span>
+          </v-col>
         </v-row>
       </v-list-item>
     </v-card-actions>
