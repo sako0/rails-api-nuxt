027d78fe7544a458ba14c70932bfe1a8661aa584
kodakoda0921 <y.m1911a1.0921@gmail.com>
2021-03-02 17:34:46 +0900

    [fix]host名をローカルと本番環境で動的に変更できるように修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   # modelでurl_forなどを使う場合はこちらを指定しないと上手くいかない
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  Rails.application.routes.default_url_options[:host] = ENV['RAILS_HOST_URL']
   config.hosts << "api.study-meeting.com"
   config.hosts << "localhost"
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
