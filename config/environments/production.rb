a2dec78842b53b8bcfb3f311878db70dbc775e32
sako0 <y.m1911a1.0921@gmail.com>
2021-11-25 18:00:47 +0900

    passwordを変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  # modelでurl_forなどを使う場合はこちらを指定しないと上手くいかない
+  Rails.application.routes.default_url_options[:host] = ENV['RAILS_HOST_URL']
+  config.hosts << "api-rails-app-nuxt-alb-1356766250.ap-northeast-1.elb.amazonaws.com"
 end
