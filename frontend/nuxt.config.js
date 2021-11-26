5bcf889bb34a50b3819a9e2f54d1ba920f65f496
sako0 <y.m1911a1.0921@gmail.com>
2021-11-27 00:33:50 +0900

    twitterログイン機能の追加

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -61,6 +61,7 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     '@nuxtjs/auth-next',
+    '@nuxtjs/firebase',
     'nuxt-webfontloader'
   ],
   webfontloader: {
@@ -144,6 +145,23 @@ export default {
           logout: false,
           clientId: true,
         },
+      },
+    },
+  },
+  firebase: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSSAGE_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    },
+    services: {
+      auth: {
+        ssr: true, // default
       },
     },
   },
